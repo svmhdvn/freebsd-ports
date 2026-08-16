e53c155ab7ebd66ed60dcb66b9fe779a389ce276
Siva Mahadevan <siva@FreeBSD.org>
2026-08-16 15:47:00 -0400

    x11-wm/river: fix checksum and build

--- x11-wm/river/Makefile.zig
+++ x11-wm/river/Makefile.zig
@@ -1,7 +1,7 @@
-ZIG_TUPLE=	aro:github.com/Vexu/arocc/archive/5f5a050569a95ecc40a426f0c3666ae7ef987ede.tar.gz:aro-0.0.0-JSD1Qi7QNgDnfcrdEJf82v3o6MhZySjYVrtdfEf3E4Se \
-		pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
-		translate_c:codeberg.org/ziglang/translate-c/archive/57c559cf581b1fcad90494eda219f98abeb155ce.tar.gz:translate_c-0.0.0-Q_BUWlX1BgCD1wo6uo97prlp9VJ4gxAjwN_vZ7nsSjGN \
-		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.6.0.tar.gz:wayland-0.6.0-lQa1kqz8AQADQmdNJsNhLoNHcnEGEUjrOaPV-dtEnEmX \
-		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.1.tar.gz:wlroots-0.20.1-jmOlcqNVBAB3uB5oqBTzpRlwu-FmMyyZMVAWCe5kmcSt \
-		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.3.0.tar.gz:xkbcommon-0.3.0-VDqIe3K9AQB2fG5ZeRcMC9i7kfrp5m2rWgLrmdNn9azr \
-		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.4.0.tar.gz:xkbcommon-0.4.0-VDqIe0i2AgDRsok2GpMFYJ8SVhQS10_PI2M_CnHXsJJZ
+ZIG_TUPLE=	aro:codeberg.org/ifreund/arocc/archive/1bf978f1e63d4fe66a90b6637ba709e9abc3c854.tar.gz?dummy=/aro-1bf978f1e63d4fe66a90b6637ba709e9abc3c854.tar.gz:aro-0.0.0-JSD1QmbSNgDrBkBPRoDPwy1rN1ABGBa7B1NzC-lqnGxw \
+		pixman:codeberg.org/ifreund/zig-pixman/archive/v0.3.0.tar.gz?dummy=/pixman-0.3.0.tar.gz:pixman-0.3.0-LClMnz2VAAAs7QSCGwLimV5VUYx0JFnX5xWU6HwtMuDX \
+		translate_c:codeberg.org/ifreund/translate-c/archive/64570104eeed461dc221e77574cd3f1c5b7eb974.tar.gz?dummy=/translate_c-64570104eeed461dc221e77574cd3f1c5b7eb974.tar.gz:translate_c-0.0.0-Q_BUWlL1BgD7PmL27vTsccyNCOvKb-B65O5Ki1ZKm-ED \
+		wayland:codeberg.org/ifreund/zig-wayland/archive/v0.6.0.tar.gz?dummy=/wayland-0.6.0.tar.gz:wayland-0.6.0-lQa1kqz8AQADQmdNJsNhLoNHcnEGEUjrOaPV-dtEnEmX \
+		wlroots:codeberg.org/ifreund/zig-wlroots/archive/v0.20.1.tar.gz?dummy=/wlroots-0.20.1.tar.gz:wlroots-0.20.1-jmOlcqNVBAB3uB5oqBTzpRlwu-FmMyyZMVAWCe5kmcSt \
+		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.3.0.tar.gz?dummy=/xkbcommon-0.3.0.tar.gz:xkbcommon-0.3.0-VDqIe3K9AQB2fG5ZeRcMC9i7kfrp5m2rWgLrmdNn9azr \
+		xkbcommon:codeberg.org/ifreund/zig-xkbcommon/archive/v0.4.0.tar.gz?dummy=/xkbcommon-0.4.0.tar.gz:xkbcommon-0.4.0-VDqIe0i2AgDRsok2GpMFYJ8SVhQS10_PI2M_CnHXsJJZ
